600d81a3717960cff8bb4e95a60ead4a1e78e5f3
unknown <Tiagoscorticati2005@gmail.com>
2024-04-15 21:19:03 -0300

    RestApiForStudents 1.0

---
+++ src/main/java/com/RestApis/Rest/dao/UsuarioDao.java
@@ -0,0 +1,16 @@
+package com.RestApis.Rest.dao;
+
+import com.RestApis.Rest.models.Usuario;
+
+import java.util.List;
+
+public interface UsuarioDao {
+
+    List<Usuario> getUsuarios();
+
+    void eliminar(Long id);
+
+    void registrar(Usuario usuario);
+
+    Usuario obtenerUsuarioPorCredenciales(Usuario usuario);
+}
